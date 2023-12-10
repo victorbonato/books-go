0d999b13b5bb1772df5be5ec273604f063519136
Victor <victorbonatoit@gmail.com>
2023-12-10 13:03:20 -0300

    Create types representing database models

---
+++ types/types.go
@@ -0,0 +1,31 @@
+package types
+
+import "github.com/google/uuid"
+
+type Author struct {
+	ID   uuid.UUID `db:"id"`
+	Name string    `db:"name"`
+}
+
+type Book struct {
+	ID          uuid.UUID `db:"id"`
+	AuthorID    uuid.UUID `db:"author_id"`
+	Title       string    `db:"title"`
+	Description string    `db:"description"`
+}
+
+type AuthorStore interface {
+	Author(id uuid.UUID) (Author, error)
+	AllAuthors() ([]Author, error)
+	CreateAuthor(a *Author) error
+	UpdateAuthor(a *Author) error
+	DeleteAuthor(id *uuid.UUID) error
+}
+
+type BookStore interface {
+	Book(id uuid.UUID) (Book, error)
+	BooksByAuthor(authorID uuid.UUID) ([]Book, error)
+	CreateBook(b *Book) error
+	UpdateBook(b *Book) error
+	DeleteBook(id uuid.UUID) error
+}
